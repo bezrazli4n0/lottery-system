4f831e456a8b658e17aac2cc6c070427c8152b74
b3zrazli4n0 <b3zrazli4n0@gmail.com>
2025-03-23 22:38:36 +0300

    feat: add chainlink vrf, positive tests for create/buy/finish

--- src/LotterySystem.sol
+++ src/LotterySystem.sol
@@ -1,4 +1,135 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-contract LotterySystem {}
+import {VRFV2PlusWrapperConsumerBase} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFV2PlusWrapperConsumerBase.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+contract LotterySystem is VRFV2PlusWrapperConsumerBase {
+    struct Lottery {
+        uint256 deadline;
+        uint256 totalAmount;
+        bool isFinished;
+        address[] users;
+    }
+
+    mapping(uint256 => mapping(address => bool)) private alreadyParticipants;
+    mapping(uint256 => uint256) private vrfRequests;
+    Lottery[] private lotteryList;
+
+    error LotteryIsEnded();
+    error AlreadyParticipant();
+    error InvalidDuration();
+    error InvalidAmount();
+    error InvalidLottery();
+    error LotteryIsActive();
+    error LotteryAlreadyFinished();
+    error NotEnoughValueToCoverVRF(uint256 requestedAmount);
+
+    event Created(uint256 id);
+    event Bought(address buyer, uint256 amount, uint256 lotteryId);
+    event Finished(address winner, uint256 amount, uint256 lotteryId);
+
+    constructor(address vrfWrapper) VRFV2PlusWrapperConsumerBase(vrfWrapper) {}
+
+    function createLottery(uint256 duration) external returns (uint256) {
+        if (duration <= 0) revert InvalidDuration();
+
+        Lottery memory lottery;
+        lottery.deadline = block.timestamp + duration;
+
+        lotteryList.push(lottery);
+        uint256 lotteryId = lotteryList.length - 1;
+
+        emit Created(lotteryId);
+        return lotteryId;
+    }
+
+    function buy(uint256 lotteryId) external payable {
+        if (msg.value <= 0) revert InvalidAmount();
+
+        if (!isLotteryExist(lotteryId)) {
+            revert InvalidLottery();
+        }
+
+        if (!isLotteryActive(lotteryId)) {
+            revert LotteryIsEnded();
+        }
+
+        if (isAddressParticipant(msg.sender, lotteryId)) {
+            revert AlreadyParticipant();
+        }
+
+        if (isLotteryFinished(lotteryId)) {
+            revert LotteryAlreadyFinished();
+        }
+
+        Lottery storage lottery = lotteryList[lotteryId];
+
+        lottery.users.push(msg.sender);
+        lottery.totalAmount += msg.value;
+        alreadyParticipants[lotteryId][msg.sender] = true;
+
+        emit Bought(msg.sender, msg.value, lotteryId);
+    }
+
+    function requestFinish(uint256 lotteryId) external payable returns (uint256) {
+        if (!isLotteryExist(lotteryId)) {
+            revert InvalidLottery();
+        }
+
+        if (isLotteryActive(lotteryId)) {
+            revert LotteryIsActive();
+        }
+
+        if (isLotteryFinished(lotteryId)) {
+            revert LotteryAlreadyFinished();
+        }
+
+        Lottery storage lottery = lotteryList[lotteryId];
+        lottery.isFinished = true;
+
+        bytes memory extraArgs = VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: true}));
+        (uint256 requestId, uint256 requestPriceInNative) = requestRandomnessPayInNative(30000, 3, 1, extraArgs);
+        if (msg.value < requestPriceInNative) {
+            revert NotEnoughValueToCoverVRF(requestPriceInNative);
+        }
+
+        uint256 refundAmount = msg.value - requestPriceInNative;
+        payable(msg.sender).transfer(refundAmount);
+
+        vrfRequests[requestId] = lotteryId;
+        return requestId;
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        uint256 seed = randomWords[0];
+        uint256 lotteryId = vrfRequests[requestId];
+
+        Lottery storage lottery = lotteryList[lotteryId];
+        address winner = lottery.users[seed % lottery.users.length];
+        uint256 amount = lottery.totalAmount;
+
+        payable(winner).transfer(amount);
+        emit Finished(winner, amount, lotteryId);
+    }
+
+    function getLottery(uint256 lotteryId) public view returns (Lottery memory) {
+        return lotteryList[lotteryId];
+    }
+
+    function isLotteryExist(uint256 lotteryId) public view returns (bool) {
+        return lotteryId >= 0 && lotteryId < lotteryList.length;
+    }
+
+    function isAddressParticipant(address addr, uint256 lotteryId) public view returns (bool) {
+        return alreadyParticipants[lotteryId][addr];
+    }
+
+    function isLotteryActive(uint256 lotteryId) public view returns (bool) {
+        return block.timestamp < lotteryList[lotteryId].deadline;
+    }
+
+    function isLotteryFinished(uint256 lotteryId) public view returns (bool) {
+        return lotteryList[lotteryId].isFinished;
+    }
+}
---
+++ test/LotterySystem.t.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import {Test} from "forge-std/Test.sol";
+import {LotterySystem} from "../src/LotterySystem.sol";
+
+contract LotterySystemTest is Test {
+    LotterySystem lotterySystem;
+    uint256 baseFork;
+    uint256 constant MINUTE_DURATION = 60;
+    address constant ALICE = address(0x1);
+    address constant BOB = address(0x2);
+    address constant JACK = address(0x3);
+    address constant BASE_VRF_WRAPPER = address(0xb0407dbe851f8318bd31404A49e658143C982F23);
+
+    function setUp() public {
+        string memory rpcUrl = vm.envString("BASE_RPC_URL");
+        uint256 blockNumber = vm.envUint("BASE_BLOCK_NUM");
+
+        baseFork = vm.createSelectFork(rpcUrl, blockNumber);
+        lotterySystem = new LotterySystem(BASE_VRF_WRAPPER);
+    }
+
+    function test_createLottery() public {
+        assert(!lotterySystem.isLotteryExist(0));
+
+        uint256 lotteryId = lotterySystem.createLottery(MINUTE_DURATION);
+
+        assert(lotterySystem.isLotteryExist(lotteryId));
+        assertEq(lotteryId, 0);
+    }
+
+    function test_buy() public {
+        uint256 lotteryId = lotterySystem.createLottery(MINUTE_DURATION);
+        assertEq(lotterySystem.getBalance(), 0);
+
+        hoax(ALICE, 100 ether);
+        lotterySystem.buy{value: 1 ether}(lotteryId);
+
+        LotterySystem.Lottery memory lottery = lotterySystem.getLottery(lotteryId);
+        assertEq(lotterySystem.getBalance(), 1 ether);
+        assertEq(lottery.totalAmount, 1 ether);
+    }
+
+    function test_requestFinish() public {
+        uint256 lotteryId = lotterySystem.createLottery(MINUTE_DURATION);
+        assertEq(lotterySystem.getBalance(), 0);
+
+        hoax(ALICE, 100 ether);
+        lotterySystem.buy{value: 1 ether}(lotteryId);
+        assertEq(lotterySystem.getBalance(), 1 ether);
+
+        hoax(BOB, 100 ether);
+        lotterySystem.buy{value: 2 ether}(lotteryId);
+        assertEq(lotterySystem.getBalance(), 3 ether);
+
+        skip(MINUTE_DURATION);
+
+        assert(!lotterySystem.getLottery(lotteryId).isFinished);
+        hoax(JACK, 100 ether);
+        uint256 requestId = lotterySystem.requestFinish{value: 1 ether}(lotteryId);
+        assert(lotterySystem.getLottery(lotteryId).isFinished);
+
+        uint256[] memory randomWords = new uint256[](1);
+        randomWords[0] = 1337;
+
+        uint256 bobBalanceBefore = BOB.balance;
+        vm.prank(BASE_VRF_WRAPPER);
+        lotterySystem.rawFulfillRandomWords(requestId, randomWords);
+        uint256 bobBalanceAfter = BOB.balance;
+
+        assertEq(lotterySystem.getBalance(), 0);
+        assertEq(bobBalanceAfter, bobBalanceBefore + 3 ether);
+        assertEq(ALICE.balance, 99 ether);
+    }
+}
